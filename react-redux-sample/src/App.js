d150d05e5c58329c23e2ee3edfbf9c80c4ee3c4c
Archana <archana462007@gmail.com>
2018-09-05 19:56:50 +0400

    first commit

---
+++ react-redux-sample/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+    constructor() {
+    super();
+    this.state = {
+      articles: [
+        { title: "React Redux Tutorial for Beginners", id: 1 },
+        { title: "Redux e React: cos'è Redux e come usarlo con React", id: 2 }
+      ]
+    };
+  }
+  render() {
+    const { articles } = this.state;
+    return <ul>{articles.map(el => <li key={el.id}>{el.title}</li>)}</ul>;
+  }
+}
+
+export default App;
